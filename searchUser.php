00c3ebb911eb8e19805f52ab37690cffc1b9e4f2
Alessandro Ricci <alessandro.ricci38@studio.unibo.it>
2024-05-04 11:59:26 +0200

    Fixed bug in search user function

--- searchUser.php
+++ searchUser.php
@@ -6,7 +6,7 @@ if(isset($_POST["query"])) {
     $output = "<ul style='list-style: none'>";
     $result = $db->searchUser($_POST["query"]);
 
-    if($result > 0) {
+    if(mysqli_num_rows($result) > 0) {
         foreach($result as $user){
             $output .= "<li><a href='./profile.php?username=" . $user['username'] . "'>" . $user['username'] . "</a></li>";
         }
