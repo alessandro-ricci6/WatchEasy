85dbb4056f021de043ecb8a09a4abaeafd6d78d9
Alessandro Ricci <alessandro.ricci38@studio.unibo.it>
2024-08-08 19:33:23 +0200

    Fixed navbar

--- template/base.php
+++ template/base.php
@@ -1,72 +1,87 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style/style.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style/style.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
+    integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=="
+    crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+    crossorigin="anonymous"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 
-    <title><?php echo $templateParams['titolo']; ?></title>
+  <title><?php echo $templateParams['titolo']; ?></title>
 </head>
-<body>
-    <!-- Header -->
-    <header>
-        <nav class="navbar navbar-expand navbar-expand-lg bg-dark">
-            <div class="container-fluid">
-              <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <button class="menuBtn me-auto mb-2 mb-lg-0 btn btn-dark" type="button" data-bs-toggle="offcanvas" data-bs-target="#staticBackdrop" aria-controls="staticBackdrop">
-                    <span class="fa-solid fa-bars"></span>
-                </button>
-                <h1 class="mx-3"><a class="text-decoration-none text-light" href="index.php">WatchEasy</a></h1>
-                <button type="button" class="btn btn-dark me-2 position-relative border border-1" data-bs-toggle="modal" data-bs-target="#notificationModal">
-                  <span class="fa-solid fa-envelope"></span>
-                  <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger" id="notificationCounter">
-                    <?php echo mysqli_num_rows($db->getActiveNotification($_SESSION['user_id'])); ?>
-                    <span class="visually-hidden">unread messages</span>
-                  </span>
-                </button>
-                
-              </div>
-            </div>
-          </nav>
-    </header>
 
-    <!-- Menu -->
-    <div class="offcanvas offcanvas-start" data-bs-backdrop="static" tabindex="-1" id="staticBackdrop" aria-labelledby="staticBackdropLabel">
-      <div class="offcanvas-header bg-dark">
-        <h2 class="offcanvas-title text-light" id="staticBackdropLabel">Menu</h2>
-        <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-      </div>
-      <div class="offcanvas-body bg-dark">
-        <div>
-          <nav class="menu">
-            <ul class="list-unstyled px-3">
-              <li class="pt-2"><a <?php isActive("index.php"); ?> href="index.php">Home</a></li>
-              <li class="pt-2"><a <?php isActive("profile.php"); ?> href="profile.php?username=<?php echo $_SESSION['username'] ?>">Profilo</a></li>
-              <li class="pt-2"><a <?php isActive("feed_page.php"); ?> href="feed_page.php?username=<?php echo $_SESSION['username']?>">Feed</a></li>
-            </ul>
-          </nav>
-          
+<body>
+  <!-- Header -->
+  <header>
+    <nav class="navbar navbar-expand navbar-expand-lg fixed-top bg-dark">
+      <div class="container-fluid">
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <button class="menuBtn me-auto mb-2 mb-lg-0 btn btn-dark" type="button" data-bs-toggle="offcanvas"
+            data-bs-target="#staticBackdrop" aria-controls="staticBackdrop">
+            <span class="fa-solid fa-bars"></span>
+          </button>
+          <h1 class="mx-3"><a class="text-decoration-none text-light" href="index.php">WatchEasy</a></h1>
+          <button type="button" class="btn btn-dark me-2 position-relative border border-1" data-bs-toggle="modal"
+            data-bs-target="#notificationModal">
+            <span class="fa-solid fa-envelope"></span>
+            <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger"
+              id="notificationCounter">
+              <?php echo mysqli_num_rows($db->getActiveNotification($_SESSION['user_id'])); ?>
+              <span class="visually-hidden">unread messages</span>
+            </span>
+          </button>
+
         </div>
       </div>
-      <div class="bg-dark d-flex justify-content-center">
-        <a class="text-danger link-underline link-underline-opacity-0 bg-dark pb-2" href="login/logout.php">Log Out</a>
+    </nav>
+  </header>
+
+  <!-- Menu -->
+  <div class="offcanvas offcanvas-start" data-bs-backdrop="static" tabindex="-1" id="staticBackdrop"
+    aria-labelledby="staticBackdropLabel">
+    <div class="offcanvas-header bg-dark">
+      <h2 class="offcanvas-title text-light" id="staticBackdropLabel">Menu</h2>
+      <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+    </div>
+    <div class="offcanvas-body bg-dark">
+      <div>
+        <nav class="menu">
+          <ul class="list-unstyled px-3">
+            <li class="pt-2"><a <?php isActive("index.php"); ?> href="index.php">Home</a></li>
+            <li class="pt-2"><a <?php isActive("profile.php"); ?>
+                href="profile.php?username=<?php echo $_SESSION['username'] ?>">Profilo</a></li>
+            <li class="pt-2"><a <?php isActive("feed_page.php"); ?>
+                href="feed_page.php?username=<?php echo $_SESSION['username'] ?>">Feed</a></li>
+          </ul>
+        </nav>
+
       </div>
     </div>
-
-      <?php
-      if(isset($templateParams['nome'])){
-          require($templateParams['nome']);
-      }
-      ?>
-
-    <!-- Modal opened when press Notification button -->
-    <div class="modal fade" id="notificationModal" tabindex="-1" aria-hidden="true">
-      <?php require 'notification-view.php'; ?>
+    <div class="bg-dark d-flex justify-content-center">
+      <a class="text-danger link-underline link-underline-opacity-0 bg-dark pb-2" href="login/logout.php">Log Out</a>
     </div>
+  </div>
+  <div style="margin-top:100px">
+    <?php
+    if (isset($templateParams['nome'])) {
+      require ($templateParams['nome']);
+    }
+    ?>
+  </div>
+
+  <!-- Modal opened when press Notification button -->
+  <div class="modal fade" id="notificationModal" tabindex="-1" aria-hidden="true">
+    <?php require 'notification-view.php'; ?>
+  </div>
 
 </body>
+
 </html>
